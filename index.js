35d61627063d7cde565fce9f749f05bbdd84b2b1
Ryan <ryanpaynt@uncc.edu>
2021-06-18 08:46:31 -0400

    Created a method that can show all departments

--- index.js
+++ index.js
@@ -10,13 +10,50 @@ const connection = mysql.createConnection({
 });
 
 const promptOne = () => {
-    
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'action_list',
+                choices:
+                    [
+                        'View Employees',
+                        'View Employees by Department',
+                        'Add Employee',
+                        'Remove Employee',
+                        'Update Employee Role',
+                        'Add Role',
+                        'Quit'
+                    ]
+            },
+        ])
+        .then((res) => {
+            switch (res.action_list) {
+                case 'View Employees':
+                    viewAllEmployees();
+                    break;
+                case 'View Employees by Department':
+                    viewByDep();
+                    break;
+                case 'Add Employee':
+                    break;
+                case 'Remove Employee':
+                    break;
+                case 'Update Employee Role':
+                    break;
+                case 'Add Role':
+                    break;
+                default:
+                    break;
+            }
+        })
 }
 
 const viewAllEmployees = () => {
     console.log('Showing all results...\n');
-    const query = 
-    `SELECT e.id, e.f_name, e.l_name, r.title, d.name AS department, r.salary, CONCAT(m.f_name, ' ', m.l_name) AS manager
+    const query =
+        `SELECT e.id, e.f_name, e.l_name, r.title, d.name AS department, r.salary, CONCAT(m.f_name, ' ', m.l_name) AS manager
     FROM employee e
     LEFT JOIN role r
       ON e.role_id = r.id
@@ -24,17 +61,33 @@ const viewAllEmployees = () => {
     ON d.id = r.department_id
     LEFT JOIN employee m
       ON m.id = e.manager_id`;
-    connection.query(query, (err,res) => {
-        if(err) throw err;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
         console.table(res);
         console.table("Employees generated.");
+        promptOne();
     });
 };
 
-
+const viewByDep = () => {
+    console.log("Viewing EMployees by Department...\n");
+    const query =
+        `SELECT d.id, d.name AS budget
+    FROM employee e
+    LEFT JOIN role r
+        ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        console.table("Employees are Shown by Department.");
+    })
+}
 
 connection.connect((err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("initializing...");
-    viewAllEmployees();
+    promptOne();
 });
